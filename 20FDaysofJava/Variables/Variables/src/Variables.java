9eab4612d0ba69a2a73416b83484290ed21245e3
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-31 13:04:51 -0500

    Added Some Variables

---
+++ 20FDaysofJava/Variables/Variables/src/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        final int a = 25;
+        final float yearsOld = 2024 - 1999;
+        final double hoursPayment = 11 * 15; // 165
+
+        String message = "I";
+        message += " Hate My Job";
+        message += " Already";
+
+        System.out.println("Message: " + message);
+
+        String workJob = "Customer Service";
+
+
+    }
+}
