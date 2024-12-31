4d2f77933aa4a6035523e635587bfb8ead2b244b
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-31 13:16:18 -0500

    Added Some Data Tyoes for Java Develeopment

---
+++ 20FDaysofJava/DataTypes/MainDataTypes.java
@@ -0,0 +1,22 @@
+public class MainDataTypes {
+
+    public static void main(String[] args) {
+
+        int YearsOld = 2024 - 1999;
+        short daysLeft = 1;
+        int milesRan = 2;
+        long yearsInDays = 365;
+
+        float a = 25.0f;
+        boolean isAdult = true;
+
+        double dYearsOld = 25.0d;
+        char cLetterName = 'R';
+        String nameInitials = "RJ";
+
+
+
+
+    }
+
+}
